d4c72e05f1e93e75dad6ab9aa61c4bc45d5649af
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2021-03-04 17:17:14 +0100

    LocationService added

---
+++ src/main/java/kb/weather_service_app/LocationService.java
@@ -0,0 +1,19 @@
+package kb.weather_service_app;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LocationService {
+    private final LocationRepository locationRepository;
+    Location createLocation(String city, String region, String country, Double latitude, Double longitude) {
+        Location location = new Location();
+        location.setCity(city);
+        location.setRegion(region);
+        location.setCountry(country);
+        location.setLatitude(latitude);
+        location.setLongitude(longitude);
+        return locationRepository.save(location);
+    }
+}
