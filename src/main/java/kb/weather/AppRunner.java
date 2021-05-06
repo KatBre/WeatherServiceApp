b0f628218901832511b7abda5f7fdaa08cbd3eb2
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2021-05-06 22:10:41 +0200

    dev profile added and branch rename

--- src/main/java/kb/weather/AppRunner.java
+++ src/main/java/kb/weather/AppRunner.java
@@ -4,11 +4,13 @@ import kb.weather.security.User;
 import kb.weather.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
+@Profile("DEV")
 @RequiredArgsConstructor
 @Component
 public class AppRunner implements CommandLineRunner {
@@ -19,6 +21,6 @@ public class AppRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         userRepository.deleteAll();
         userRepository.save(new User("user", passwordEncoder.encode("user1"), Collections.singletonList(() -> "ROLE_USER")));
-        userRepository.save(new User("admin", passwordEncoder.encode("admin1"), Collections.singletonList(() -> "ROLE_ADMIN")));
+        userRepository.save(new User("admin", passwordEncoder.encode("admin1"), Collections.singletonList(() -> "ROLE_ADMIN; ROLE_USER")));
     }
 }
--- src/main/java/kb/weather/location/LocationController.java
+++ src/main/java/kb/weather/location/LocationController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
